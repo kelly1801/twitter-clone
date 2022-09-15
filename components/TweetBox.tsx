7711c11cd74bccf202c2db6a75f8f993b3f6143c
kellyc <77988055+kelly1801@users.noreply.github.com>
2022-09-15 10:33:04 -0500

    add tweetBox component

---
+++ components/TweetBox.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Image from 'next/image'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
+function TweetBox() {
+    return ( 
+        <div className="flex space-x-2 p-5">
+            <Image
+                src="https://links.papareact.com/gll"
+                alt="generic user picture"
+                width={100}
+                height={100}
+
+                className="h-14 w-14 rounded-full object-cover mt-4"
+            />
+
+            <div className="flex flex-1 items-centerpl-2">
+                <form className="flex flex-1 flex-col">
+                    <input type="text" placeholder="What's Happening?"  className="h-24 w-full text-xl outline-none placeholder:text-xl"/>
+
+                    <div className="flex items-center">
+                        <div className="flex flex-1 space-x-2 text-twitter">
+                            <CameraAltOutlinedIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"  />
+                            <SearchOutlinedIcon className="h-5 w-5" />
+                            <EmojiEmotionsOutlinedIcon className="h-5 w-5" />
+                            <CalendarMonthOutlinedIcon className="h-5 w-5" />
+                            <LocationOnOutlinedIcon className="h-5 w-5" />
+                        </div>
+
+                        <button className="bg-twitter px-5 py-2 font-bold text-white rounded-full">Tweet</button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default TweetBox
